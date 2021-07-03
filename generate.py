708a547435b2feda19903e12de0af4b9f3e6ff34
Alan Dsilva <alandsilva2001@gmail.com>
2021-07-03 11:14:36 +0530

    Update text to badges

---
+++ generate.py
@@ -0,0 +1,52 @@
+languages = [
+    {
+        "message": "Dart",
+        "color": "00B4AB",
+        "logo": "dart",
+        "logoColor": "0175C2",
+    },
+    {
+        "message": "HTML",
+        "color": "e44b23",
+        "logo": "HTML5",
+        "logoColor": "E34F26",
+    },
+    {
+        "message": "JavaScript",
+        "color": "f1e05a",
+        "logo": "JavaScript",
+        "logoColor": "F7DF1E",
+    }, {
+        "message": "PHP",
+        "color": "4F5D95",
+        "logo": "PHP",
+        "logoColor": "777BB4",
+    }, {
+        "message": "CSS",
+        "color": "563d7c",
+        "logo": "CSS3",
+        "logoColor": "1572B6",
+    }, {
+        "message": "Java",
+        "color": "b07219",
+        "logo": "Java",
+        "logoColor": "007396",
+    }, {
+        "message": "Python",
+        "color": "3572A5",
+        "logo": "Python",
+        "logoColor": "3776AB",
+    },
+    {
+        "message": "C",
+        "color": "555555",
+        "logo": "C",
+        "logoColor": "A8B9CC",
+    },
+]
+
+
+for lang in languages:
+    template = "![{name}](https://img.shields.io/static/v1?style=flat&label=%20&labelColor=%23555&logo={logo}&logoColor=%23{logoColor}&message={message}&color=%23{color})".format(
+        name=lang["message"], logo=lang["logo"], logoColor=lang["logoColor"], message=lang["message"], color=lang["color"])
+    print(template)
