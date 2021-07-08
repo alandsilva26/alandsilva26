e2247a60487eea8c8d04ac38313e541a36f6fc6a
Alan Dsilva <alandsilva2001@gmail.com>
2021-07-08 09:14:49 +0530

    Update Script

--- generate.py
+++ generate.py
@@ -1,3 +1,5 @@
+import random
+
 languages = [
     {
         "message": "Dart",
@@ -45,13 +47,74 @@ languages = [
     },
 ]
 
-finalString = ""
-for lang in languages:
-    # template = "![{name}](https://img.shields.io/static/v1?style=flat&label=%20&labelColor=%23555&logo={logo}&logoColor=%23{logoColor}&message={message}&color=%23{color})".format(
-    #     name=lang["message"], logo=lang["logo"], logoColor=lang["logoColor"], message=lang["message"], color=lang["color"])
-    template = "![{name}](https://img.shields.io/static/v1?style=flat&label=&message={message}&color=%23{color})".format(
-        name=lang["message"],  message=lang["message"], color=lang["color"])
+frameworks = [{
+    "message": "Flutter",
+}, {
+    "message": "React",
+}, {
+    "message": "Django",
+}, {
+    "message": "Sass",
+}, {
+    "message": "Bootstrap",
+}, {
+    "message": "Jquery",
+},
+]
 
-    finalString += template.strip() + " "
+Tools = [{
+    "message": "Docker",
+}, {
+    "message": "Git",
+}, {
+    "message": "Github Actions",
+}, {
+    "message": "Firebase",
+},
+]
 
-print(finalString)
+colors = ["brightgreen", "red", "%23563d7c",
+          "orange", "%23555555" "green", "blue", "blueviolet", ]
+
+
+# Main
+if __name__ == "__main__":
+    initColor = random.randint(0, len(colors) - 1)
+    finalString = ""
+
+    # menu
+    choice = 1
+
+    try:
+        choice = int(
+            input("1. itemuages\n2. Frameworks\n3.Tools\nEnter your choice(Default 1): "))
+    except:
+        print("No value entered")
+
+    itemList = None
+
+    if(choice == 1):
+        itemList = languages
+    elif (choice == 2):
+        itemList = frameworks
+    elif(choice == 3):
+        itemList = Tools
+    else:
+        print("Invalid choice")
+
+    if itemList != None:
+        for item in itemList:
+            color = colors[initColor]
+            initColor = (initColor + 1) % len(colors)
+            print(initColor)
+            if "color" in item:
+                color = "%23{}".format(item["color"])
+            message = item["message"].replace(" ", "%20")
+            # template = "![{name}](https://img.shields.io/static/v1?style=flat&label=%20&labelColor=%23555&logo={logo}&logoColor=%23{logoColor}&message={message}&color=%23{color})".format(
+            #     name=item["message"], logo=item["logo"], logoColor=item["logoColor"], message=item["message"], color=item["color"])
+            template = "![{name}](https://img.shields.io/static/v1?style=flat&label=&message={message}&color={color})".format(
+                name=message,  message=message, color=color)
+
+            finalString += template.strip() + " "
+
+        print(finalString)
